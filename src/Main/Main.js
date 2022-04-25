cd4579851d749b6f11ed3fb7087b20c8f9ee161d
Ian Cheifetz <sylvan.iacheifetz09@gmail.com>
2022-04-25 15:28:37 -0700

    finished product, forgot to checkout to a dev branch or push after each step

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import backgroundImg from '../background.png';
 import './Main.css';
+import { zodiac } from '../data.js';
+import ZodiacCard from '../ZodiacCard/ZodiacCard';
 
 export default function Main() {
   return (
     <main style={{ backgroundImage: `url(${backgroundImg})` }}>
-      {/* import the ZodiacCard component */}
-      {/* get the zodiac array from props */}
-      {/* for each item in the `zodiacs` array props, render a ZodiacCard component, passing in the name and the dates as props to the ZodiacCard component */}
-      {/* remember that when you map over an array and render out a list, each item in the list needs a unique 'key' prop */}
+      {zodiac.map((item) => (
+        <ZodiacCard key={item.name} {...item} />
+      ))}
     </main>
   );
 }
